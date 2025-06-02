8d53230f17c53c89e4c337fa9ab38c91b4692be8
RazzanIkmalSaputra <razzanikmalsaputra9@gmail.com>
2025-06-02 16:06:25 +0700

    menambahkan file yang belum ke uploud

---
+++ ABSTRAKSI.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+class abstraksiKlas {
+private: string x, y;
+
+public:
+
+    // method untuk mengisi nilai 
+    // private member
+    void setXY(string a, string b){
+        x = a;
+        y = b;
+    }
+    //menampilkan nilai 
+    void display(){
+        cout << "x =" << x << endl;
+        cout << "y =" << y << endl;
+    }
+};
+
+int main(){
+    abstraksiKlas ak;
+    ak.setXY("Yogyakarta", "kampus");
+    ak.display();
+
+    return 0;
+}
+
